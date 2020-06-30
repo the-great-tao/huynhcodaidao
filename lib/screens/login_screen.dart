6988be8ba37d4fd557757500ea8aada272b8e76e
The Great Tao <nmnhat8668@gmail.com>
2020-06-30 15:59:42 +0700

    Update login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,130 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_widgets/responsive_widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Theme(
-        data: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.white,
-          accentColor: Colors.white,
-          cursorColor: Colors.white,
-          buttonColor: Colors.amber.withAlpha(240),
-          textTheme: TextTheme(
-            button: TextStyle(
-              fontSize: 18,
+    ResponsiveWidgets.init(
+      context,
+      width: 1080,
+      height: 1920,
+      allowFontScaling: true,
+    );
+
+    return ResponsiveWidgets.builder(
+      child: Scaffold(
+        body: Theme(
+          data: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.white,
+            accentColor: Colors.white,
+            cursorColor: Colors.white,
+            buttonColor: Colors.amber.withAlpha(240),
+            textTheme: TextTheme(
+              button: TextStyle(
+                fontSize: ScreenUtil().setSp(50),
+              ),
+            ),
+            inputDecorationTheme: InputDecorationTheme(
+              labelStyle: TextStyle(
+                fontSize: ScreenUtil().setSp(50),
+              ),
+              contentPadding: EdgeInsetsResponsive.all(50),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(9999),
+                borderSide: BorderSide(
+                  width: 4.w,
+                  color: Colors.white,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(9999),
+                borderSide: BorderSide(
+                  width: 8.w,
+                  color: Colors.white,
+                ),
+              ),
             ),
           ),
-          inputDecorationTheme: InputDecorationTheme(
-            labelStyle: TextStyle(
-              fontSize: 18,
-            ),
-            contentPadding: EdgeInsets.all(20),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(9999),
-              borderSide: BorderSide(
-                width: 1,
-                color: Colors.white,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(9999),
-              borderSide: BorderSide(
-                width: 2,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/login_screen_background.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.fromLTRB(80, 60, 80, 60),
-                  child: Image(
-                    image: AssetImage('assets/login_screen_logo.png'),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(9999),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.amberAccent,
-                        blurRadius: 30,
-                        spreadRadius: 30,
-                      ),
-                    ],
-                  ),
+          child: SingleChildScrollView(
+            child: ContainerResponsive(
+              width: 1080,
+              height: 1920,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/login_screen_background.png'),
+                  fit: BoxFit.fill,
                 ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    style: TextStyle(
-                      fontSize: 18,
+              ),
+              child: Column(
+                children: <Widget>[
+                  ContainerResponsive(
+                    width: 540,
+                    heightResponsive: false,
+                    margin: EdgeInsetsResponsive.all(200),
+                    child: Image(
+                      image: AssetImage('assets/login_screen_logo.png'),
                     ),
-                    decoration: InputDecoration(
-                      labelText: 'Số điện thoại',
-                      prefixIcon: Icon(
-                        Icons.phone,
-                      ),
-                      prefixIconConstraints: BoxConstraints(
-                        minWidth: 80,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                  child: TextFormField(
-                    obscureText: true,
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                    decoration: InputDecoration(
-                      labelText: 'Mật khẩu',
-                      prefixIcon: Icon(
-                        Icons.vpn_key,
-                      ),
-                      prefixIconConstraints: BoxConstraints(
-                        minWidth: 80,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(40, 40, 40, 100),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(9999),
-                  ),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: RaisedButton(
-                      padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(9999),
-                        side: BorderSide(
-                          color: Colors.amber,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9999),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.amberAccent,
+                          blurRadius: 80.w,
+                          spreadRadius: 80.w,
                         ),
+                      ],
+                    ),
+                  ),
+                  ContainerResponsive(
+                    margin: EdgeInsetsResponsive.fromLTRB(120, 20, 120, 20),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                        fontSize: ScreenUtil().setSp(50),
                       ),
-                      onPressed: () {},
-                      child: Text(
-                        'ĐĂNG NHẬP',
-                        style: TextStyle(
-                          color: Colors.black54,
+                      decoration: InputDecoration(
+                        labelText: 'Số điện thoại',
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          size: ScreenUtil().setSp(80),
+                        ),
+                        prefixIconConstraints: BoxConstraints(
+                          minWidth: 200.w,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  ContainerResponsive(
+                    margin: EdgeInsetsResponsive.fromLTRB(120, 20, 120, 20),
+                    child: TextFormField(
+                      obscureText: true,
+                      style: TextStyle(
+                        fontSize: ScreenUtil().setSp(50),
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'Mật khẩu',
+                        prefixIcon: Icon(
+                          Icons.vpn_key,
+                          size: ScreenUtil().setSp(80),
+                        ),
+                        prefixIconConstraints: BoxConstraints(
+                          minWidth: 200.w,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ContainerResponsive(
+                    padding: EdgeInsetsResponsive.fromLTRB(120, 60, 120, 60),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9999),
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        padding:
+                            EdgeInsetsResponsive.fromLTRB(120, 60, 120, 60),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(9999),
+                          side: BorderSide(
+                            color: Colors.amber,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'ĐĂNG NHẬP',
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
