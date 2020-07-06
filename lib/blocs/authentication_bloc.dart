596fe4e4a31a4734cdd0dc2ac7e9dfc1167df756
The Great Tao <nmnhat8668@gmail.com>
2020-07-06 18:56:06 +0700

    Add Authentication{Event, State, Bloc}

---
+++ lib/blocs/authentication_bloc.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+import 'package:huynhcodaidao/blocs/authentication_event.dart';
+import 'package:huynhcodaidao/blocs/authentication_state.dart';
+import 'package:huynhcodaidao/repositories/user_repository.dart';
+import 'package:huynhcodaidao/services/user_service.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository})
+      : assert(userRepository != null),
+        super(AuthenticationInitial());
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AuthenticationStarted) {
+      yield AuthenticationInProgress();
+      final UserToken userToken = await userRepository.getToken();
+      await Future.delayed(Duration(seconds: 3));
+
+      if (userToken != null) {
+        yield AuthenticationSuccess();
+      } else {
+        yield AuthenticationFailure();
+      }
+    }
+
+    if (event is AuthenticationLoggedIn) {
+      yield AuthenticationInProgress();
+      await userRepository.putToken(event.userToken);
+      yield AuthenticationSuccess();
+    }
+
+    if (event is AuthenticationLoggedOut) {
+      yield AuthenticationInProgress();
+      await userRepository.deleteToken();
+      yield AuthenticationFailure();
+    }
+  }
+}
