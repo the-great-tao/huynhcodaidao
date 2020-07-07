b568ce80ea902511acad4367904b230abc3058f0
The Great Tao <nmnhat8668@gmail.com>
2020-07-07 22:26:57 +0700

    Update HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_widgets/responsive_widgets.dart';
+
+import 'package:huynhcodaidao/ui_components/linear_gradients.dart';
+import 'package:huynhcodaidao/widgets/app_bar_01_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    ResponsiveWidgets.init(
+      context,
+      width: 1080,
+      height: 1920,
+      allowFontScaling: true,
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradients.main,
+      ),
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar01Widget(
+            height: 160.h,
+          ),
+          body: Container(
+            child: Center(
+              child: Text('Home Screen'),
+            ),
+          ),
+        ),
       ),
     );
   }
