35214bd2bf88f586af8854fe0c230c4f573fd9f6
The Great Tao <nmnhat8668@gmail.com>
2020-07-12 17:39:03 +0700

    Update UserRepository

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,36 +1,36 @@
+import 'package:get_it/get_it.dart';
 import 'package:meta/meta.dart';
 import 'package:hive/hive.dart';
-import 'package:dio/dio.dart';
 
 import 'package:huynhcodaidao/models/user_token.dart';
 
 import 'package:huynhcodaidao/services/user_service.dart';
 
+final GetIt getIt = GetIt.instance;
+
 class UserRepository {
   final Box _appData = Hive.box('appData');
-  final UserService _userService = UserService(Dio());
+  final UserService _userService = getIt.get<UserService>();
 
   Future<UserToken> authenticate({
     @required String username,
     @required String password,
-  }) async {
-    UserToken userToken = await _userService.authenticate(
+  }) {
+    return _userService.authenticate(
       username: username,
       password: password,
     );
-
-    return userToken;
   }
 
-  Future<void> putToken(UserToken userToken) async {
-    await _appData.put('userToken', userToken);
+  Future<void> putToken(UserToken userToken) {
+    return _appData.put('userToken', userToken);
   }
 
   Future<UserToken> getToken() async {
     return _appData.get('userToken');
   }
 
-  Future<void> deleteToken() async {
-    await _appData.delete('userToken');
+  Future<void> deleteToken() {
+    return _appData.delete('userToken');
   }
 }
