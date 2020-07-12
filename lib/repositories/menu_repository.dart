5111978b82bc7a413583c2d141e673e4f43e26d2
The Great Tao <nmnhat8668@gmail.com>
2020-07-12 17:47:56 +0700

    Update MenuRepository

--- lib/repositories/menu_repository.dart
+++ lib/repositories/menu_repository.dart
@@ -1,25 +1,26 @@
+import 'package:get_it/get_it.dart';
 import 'package:meta/meta.dart';
 import 'package:hive/hive.dart';
-import 'package:dio/dio.dart';
 
 import 'package:huynhcodaidao/models/user_token.dart';
 import 'package:huynhcodaidao/models/menu.dart';
 
 import 'package:huynhcodaidao/services/menu_service.dart';
 
+final GetIt getIt = GetIt.instance;
+
 class MenuRepository {
   final Box _appData = Hive.box('appData');
-  final MenuService _menuService = MenuService(Dio());
+  final MenuService _menuService = getIt.get<MenuService>();
 
   Future<Menu> get({
     @required String slug,
   }) async {
     UserToken userToken = _appData.get('userToken');
-    Menu menu = await _menuService.get(
+
+    return _menuService.get(
       token: 'Bearer ' + userToken.accessToken,
       slug: slug,
     );
-
-    return menu;
   }
 }
