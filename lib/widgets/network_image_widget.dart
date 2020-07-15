1dcb14bc6e369c854fcef2180acab8a58f243bb7
The Great Tao <nmnhat8668@gmail.com>
2020-07-16 00:44:36 +0700

    Update NetworkImageWidget

--- lib/widgets/network_image_widget.dart
+++ lib/widgets/network_image_widget.dart
@@ -1,29 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_widgets/responsive_widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class NetworkImageWidget extends StatelessWidget {
   final String source;
+  final Map<String, String> headers;
   final double width;
   final double height;
   final BoxFit fit;
-  final Map<String, String> headers;
 
   const NetworkImageWidget({
     Key key,
     this.source,
+    this.headers,
     this.width,
     this.height,
     this.fit,
-    this.headers,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      source,
+    return CachedNetworkImage(
+      imageUrl: source,
+      httpHeaders: headers,
       width: width,
       height: height,
       fit: fit,
-      headers: headers,
+      fadeOutDuration: Duration(milliseconds: 500),
+      placeholder: (BuildContext context, String url) {
+        print(url);
+        return Container(
+          width: width,
+          height: height,
+          child: Center(
+            child: SpinKitFadingCircle(
+              size: 120.sp,
+              color: Colors.amber,
+            ),
+          ),
+        );
+      },
+      errorWidget: (BuildContext context, String url, dynamic error) {
+        return Container(
+          width: width,
+          height: height,
+        );
+      },
     );
   }
 }
