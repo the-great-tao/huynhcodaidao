6e826decdfa929f805deadd370c3e948aa4b61f9
The Great Tao <nmnhat8668@gmail.com>
2020-07-15 23:03:40 +0700

    Update PhotoAlbumWidget

--- lib/widgets/photo_album_widget.dart
+++ lib/widgets/photo_album_widget.dart
@@ -2,12 +2,12 @@ import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 import 'package:responsive_widgets/responsive_widgets.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:frefresh/frefresh.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:huynhcodaidao/models/user_token.dart';
 import 'package:huynhcodaidao/models/photo_album_item.dart';
@@ -15,6 +15,8 @@ import 'package:huynhcodaidao/models/photo_album_page.dart';
 import 'package:huynhcodaidao/models/photo_album.dart';
 
 import 'package:huynhcodaidao/repositories/photo_album_repository.dart';
+
+import 'package:huynhcodaidao/ui_components/linear_gradients.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -34,7 +36,7 @@ class _PhotoAlbumWidgetState extends State<PhotoAlbumWidget> {
   final Box _appData = Hive.box('appData');
   final PhotoAlbumRepository _photoAlbumRepository =
       getIt.get<PhotoAlbumRepository>();
-  final RefreshController _refreshController = RefreshController();
+  final PageController _photoPageController = PageController();
 
   dynamic _state;
   Future<PhotoAlbum> _photoAlbumFuture;
@@ -42,6 +44,7 @@ class _PhotoAlbumWidgetState extends State<PhotoAlbumWidget> {
   PhotoAlbumPage _photoAlbumPage;
   List<PhotoAlbumItem> _photoAlbumItems;
   int _page = 1;
+  int _photoIndex = 0;
   bool _shouldLoad = false;
 
   @override
@@ -55,9 +58,28 @@ class _PhotoAlbumWidgetState extends State<PhotoAlbumWidget> {
 
   @override
   void dispose() {
-    _refreshController.dispose();
+    _photoPageController.dispose();
 
     super.dispose();
+  }
+
+  void onPhotoPageChanged(int index) {
+    _photoIndex = index;
+
+    if (_state is LoadState && _state == LoadState.LOADING) {
+      return;
+    }
+    if (!_shouldLoad || _photoAlbumItems.length - _photoIndex >= 3) {
+      return;
+    }
+
+    _state = LoadState.LOADING;
+    _photoAlbumFuture = _photoAlbumRepository.get(
+      path: widget.actionUrl,
+      page: _page + 1,
+    );
+
+    setState(() {});
   }
 
   @override
@@ -75,19 +97,16 @@ class _PhotoAlbumWidgetState extends State<PhotoAlbumWidget> {
         }
 
         if (snapshot.connectionState == ConnectionState.done) {
-          if (_state == null || _state is RefreshState) {
+          if (_state == null) {
             _photoAlbum = snapshot.data;
             _photoAlbumPage = _photoAlbum.photoAlbumPage;
             _photoAlbumItems = _photoAlbumPage.data;
 
             _page = 1;
             _shouldLoad = _photoAlbumPage.nextPageUrl != null;
-            if (_state is RefreshState) {
-              _refreshController.refreshCompleted();
-            }
           }
 
-          if (_state is LoadState) {
+          if (_state is LoadState && _state == LoadState.LOADING) {
             PhotoAlbum _nextPhotoAlbum = snapshot.data;
             PhotoAlbumPage _nextPhotoAlbumPage = _nextPhotoAlbum.photoAlbumPage;
             List<PhotoAlbumItem> _nextPhotoAlbumItems =
@@ -100,74 +119,109 @@ class _PhotoAlbumWidgetState extends State<PhotoAlbumWidget> {
 
             _page++;
             _shouldLoad = _nextPhotoAlbumPage.nextPageUrl != null;
-            _refreshController.loadComplete();
           }
+
+          _state = LoadState.IDLE;
         }
 
-        return SmartRefresher(
-          enablePullDown: true,
-          enablePullUp: true,
-          controller: _refreshController,
-          scrollDirection: Axis.horizontal,
-          physics: PageScrollPhysics(),
-          footer: ClassicFooter(
-            iconPos: IconPosition.top,
-            outerBuilder: (child) {
-              return Container(
-                child: Center(
-                  child: child,
-                ),
-              );
-            },
-          ),
-          header: ClassicHeader(
-            iconPos: IconPosition.top,
-            outerBuilder: (child) {
-              return Container(
-                child: Center(
-                  child: child,
-                ),
-              );
-            },
-          ),
-          onRefresh: () {
-            _state = RefreshState.REFRESHING;
-            _photoAlbumFuture = _photoAlbumRepository.get(
-              path: widget.actionUrl,
-            );
-            setState(() {});
-          },
-          onLoading: () {
-            _state = LoadState.LOADING;
-            _photoAlbumFuture = _photoAlbumRepository.get(
-              path: widget.actionUrl,
-              page: _page + 1,
-            );
-            setState(() {});
-          },
-          child: ListView.builder(
-            itemCount: _photoAlbumItems == null || _photoAlbumItems.length == 0
-                ? 0
-                : _photoAlbumPage.to - _photoAlbumPage.from + 1,
-            itemBuilder: (BuildContext context, int index) {
-              PhotoAlbumItem _photoAlbumItem = _photoAlbumItems[index];
+        return Column(
+          children: <Widget>[
+            Expanded(
+              child: ClipRect(
+                child: PhotoViewGallery.builder(
+                  pageController: _photoPageController,
+                  onPageChanged: onPhotoPageChanged,
+                  itemCount:
+                      _photoAlbumItems == null || _photoAlbumItems.length == 0
+                          ? 0
+                          : _photoAlbumPage.to - _photoAlbumPage.from + 1,
+                  builder: (BuildContext context, int index) {
+                    PhotoAlbumItem _photoAlbumItem = _photoAlbumItems[index];
 
-              return Container(
-                width: 1080.sp,
-                color: Colors.black,
-                child: PhotoView(
-                  backgroundDecoration: BoxDecoration(color: Colors.black),
-                  imageProvider: CachedNetworkImageProvider(
-                    _photoAlbumItem.photoUrl,
-                    headers: {
-                      'Authorization': 'Bearer ' +
-                          (_appData.get('userToken') as UserToken).accessToken,
-                    },
-                  ),
+                    return PhotoViewGalleryPageOptions(
+                      minScale: PhotoViewComputedScale.contained,
+                      maxScale: PhotoViewComputedScale.contained * 5,
+                      imageProvider: CachedNetworkImageProvider(
+                        _photoAlbumItem.photoUrl,
+                        headers: {
+                          'Authorization': 'Bearer ' +
+                              (_appData.get('userToken') as UserToken)
+                                  .accessToken,
+                        },
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
+              ),
+            ),
+            Container(
+              width: 1080.w,
+              height: 200.sp,
+              color: Colors.black,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(240.sp, 40.sp, 240.sp, 40.sp),
+                decoration: BoxDecoration(
+                  gradient: LinearGradients.main,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(9999.sp),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4.sp,
+                      spreadRadius: 2.sp,
+                      offset: Offset(0.sp, 2.sp),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20.sp,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        if (_photoIndex <= 0) {
+                          return;
+                        }
+
+                        _photoPageController.previousPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.decelerate,
+                        );
+                      },
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 100.sp,
+                      ),
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        if (_photoIndex >= _photoAlbumItems.length - 1) {
+                          return;
+                        }
+
+                        _photoPageController.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.decelerate,
+                        );
+                      },
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                        size: 100.sp,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20.sp,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         );
       },
     );
