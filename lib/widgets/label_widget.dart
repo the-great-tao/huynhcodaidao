8c75d2464de754628783142631aab93b976760ec
The Great Tao <nmnhat8668@gmail.com>
2020-07-16 15:23:10 +0700

    Update LabelWidget

--- lib/widgets/label_widget.dart
+++ lib/widgets/label_widget.dart
@@ -25,43 +25,55 @@ class LabelWidget extends StatefulWidget {
 class _LabelWidgetState extends State<LabelWidget> {
   final Box _appData = Hive.box('appData');
 
+  Future<Response> _responseFuture;
+
+  @override
+  void initState() {
+    _responseFuture = Dio().get(
+      widget.labelUrl,
+      options: Options(
+        headers: {
+          'Authorization':
+              'Bearer ' + (_appData.get('userToken') as UserToken).accessToken,
+        },
+      ),
+    );
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Dio().get(
-          widget.labelUrl,
-          options: Options(
-            headers: {
-              'Authorization': 'Bearer ' +
-                  (_appData.get('userToken') as UserToken).accessToken,
-            },
+      future: _responseFuture,
+      builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
+        if (!snapshot.hasData ||
+            snapshot.connectionState != ConnectionState.done) {
+          return Container();
+        }
+
+        Response response = snapshot.data;
+        Label label = Label.fromJson(response.data);
+
+        _responseFuture = null;
+
+        return Container(
+          padding: EdgeInsets.fromLTRB(20.sp, 10.sp, 20.sp, 10.sp),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(9999.sp),
           ),
-        ),
-        builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
-          if (!snapshot.hasData ||
-              snapshot.connectionState != ConnectionState.done) {
-            return Container();
-          }
-
-          Response response = snapshot.data;
-          Label label = Label.fromJson(response.data);
-
-          return Container(
-            padding: EdgeInsets.fromLTRB(20.sp, 10.sp, 20.sp, 10.sp),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(9999.sp),
-            ),
-            child: Center(
-              child: Text(
-                label.data.toString(),
-                style: GoogleFonts.robotoSlab(
-                  color: Colors.white,
-                  fontSize: 32.sp,
-                ),
+          child: Center(
+            child: Text(
+              label.data.toString(),
+              style: GoogleFonts.robotoSlab(
+                color: Colors.white,
+                fontSize: 32.sp,
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
