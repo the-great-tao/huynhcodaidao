0cee3d70aa18766f8e4e9611f34e5d4aad8ab574
The Great Tao <nmnhat8668@gmail.com>
2020-07-19 23:02:40 +0700

    Update custom AppBar widget

--- lib/widgets/app_bar_02_widget.dart
+++ lib/widgets/app_bar_02_widget.dart
@@ -1,58 +1,104 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:get_it/get_it.dart';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_widgets/responsive_widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:huynhcodaidao/blocs/audio_controller_state.dart';
+import 'package:huynhcodaidao/blocs/audio_controller_bloc.dart';
 
 import 'package:huynhcodaidao/ui_components/linear_gradients.dart';
 import 'package:huynhcodaidao/widgets/app_bar_widget.dart';
 
+final GetIt getIt = GetIt.instance;
+
 class AppBar02Widget extends StatefulWidget implements PreferredSizeWidget {
   final double height;
   final String title;
+  final double audioControllerHeight;
 
   const AppBar02Widget({
     Key key,
     @required this.height,
     @required this.title,
+    @required this.audioControllerHeight,
   })  : assert(height != null),
-        assert(title != null);
+        assert(title != null),
+        assert(audioControllerHeight != null);
 
   @override
-  Size get preferredSize => Size.fromHeight(height + 180.sp);
+  Size get preferredSize => Size.fromHeight(height + audioControllerHeight);
 
   @override
   State createState() => _AppBar02WidgetState();
 }
 
 class _AppBar02WidgetState extends State<AppBar02Widget> {
+  final AssetsAudioPlayer _assetsAudioPlayer = getIt.get<AssetsAudioPlayer>();
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Container(
-          width: 1080.w,
-          height: widget.height + 180.sp,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
-              colors: [
-//                Color.fromRGBO(208, 246, 255, 1),
-//                Color.fromRGBO(255, 237, 237, 1),
-//                Color.fromRGBO(255, 255, 231, 1),
+        BlocBuilder<AudioControllerBloc, AudioControllerState>(
+          builder: (BuildContext context, AudioControllerState state) {
+            if (state is AudioControllerHiding) {
+              return Container();
+            }
 
-                Color.fromRGBO(92, 121, 255, 1),
-                Color.fromRGBO(76, 221, 242, 1),
+            return Column(
+              children: <Widget>[
+                SizedBox(
+                  height: widget.height,
+                ),
+                Container(
+                  width: 1080.w,
+                  height: widget.audioControllerHeight,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      colors: [
+//                        Color.fromRGBO(208, 246, 255, 1),
+//                        Color.fromRGBO(255, 237, 237, 1),
+//                        Color.fromRGBO(255, 255, 231, 1),
+                        Color.fromRGBO(92, 121, 255, 1),
+                        Color.fromRGBO(76, 221, 242, 1),
+                      ],
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 8.h,
+                        spreadRadius: 2.h,
+                        offset: Offset(0, 4.h),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: _assetsAudioPlayer.builderIsPlaying(
+                      builder: (context, isPlaying) {
+                        return isPlaying
+                            ? Icon(
+                                FontAwesome.stop,
+                                color: Colors.white,
+                                size: 100.sp,
+                              )
+                            : Icon(
+                                FontAwesome.play,
+                                color: Colors.white,
+                                size: 100.sp,
+                              );
+                      },
+                    ),
+                  ),
+                ),
               ],
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 8.h,
-                spreadRadius: 2.h,
-                offset: Offset(0, 4.h),
-              ),
-            ],
-          ),
+            );
+          },
         ),
         AppBarWidget(
           height: widget.height,
